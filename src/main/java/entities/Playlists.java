8e29ac6ea6f996f7c1c244ebac7eeac0c433f750
maryam banjo <mitechnigeria@gmail.com>
2025-10-21 11:04:37 +0100

    Feature: Completed Playlist DAO and filled in my ToString methods and equalsTO method

--- src/main/java/entities/Playlists.java
+++ src/main/java/entities/Playlists.java
@@ -28,5 +28,66 @@ public class Playlists {
         this.description = description;
         this.createdAt = createdAt;
     }
+    public int getPlaylistID(){
+        return playlistID;
+    }
+    public int getUserID(){
+        return userID;
+    }
+    public String getPlaylistName(){
+        return playlistName;
+    }
 
+    public String getDescription(String description){
+        return description;
+    }
+    public Date getCreatedAt(Date createdAt) {
+        return createdAt;
+    }
+    public void setPlaylistID(int playlistID){
+        this.playlistID = playlistID;
+    }
+    public void setUserID(int userID){
+        this.userID= userID;
+    }
+    public void setPlaylistName(String playlistName){
+        this.playlistName = playlistName;
+    }
+    public void setDescription(String description){
+      this.description=description;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+        Playlists other = (Playlists) o;
+        return playlistID == other.playlistID &&
+                Objects.equals(playlistName, other.playlistName) &&
+                Objects.equals(description, other.description);
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(playlistID,playlistName, description);
+
+    }
+    @Override
+    public String toString(){
+        return "Playlists{" + "Playlist ID: "+ playlistID +
+                " ,Playlist Name: "+ playlistName +
+                " ,Playlist Description: "+ description +
+                " , User ID: " + userID +
+                " ,Created At: "+ createdAt +
+                "}";
+    }
 }
