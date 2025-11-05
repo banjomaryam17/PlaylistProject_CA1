694a014d8e0db7332a75457184986adf6104e9dc
maryam banjo <mitechnigeria@gmail.com>
2025-11-05 09:54:07 +0000

    Refactor: Added lombok abd builder annotation to keep my code less messy and consistent

--- src/main/java/entities/Genre.java
+++ src/main/java/entities/Genre.java
@@ -1,81 +1,47 @@
 package entities;
+
+import lombok.*;
 import java.util.Objects;
 
-//@author Maryam
 
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Genre implements Comparable<Genre> {
 
-public class Genre {
-//CREATE TABLE genres (
-//                        genreID INT(11) NOT NULL AUTO_INCREMENT,
-//                        genreName VARCHAR(50) NOT NULL,
-//                        description VARCHAR(255) DEFAULT NULL,
-//                        PRIMARY KEY (genreID)
-//);
+    @EqualsAndHashCode.Include
     private int genreID;
+
+    @NonNull
     private String genreName;
     private String description;
 
-    public Genre(){
-
-    }
-
-    public Genre(int genreID, String genreName, String description){
-        this.genreID = genreID;
-        this.genreName = genreName;
-        this.description = description;
-    }
-    public int getGenreID(){
-        return genreID;
-    }
-    public String getGenreName(){
-        return genreName;
-    }
-    public String getDescription(){
-        return description;
-    }
-    public void setGenreID(int genreID){
-        this.genreID = genreID;
-    }
-
-    public void setGenreName(String genreName){
-        this.genreName = genreName;
-    }
-    public void setDescription(String description){
-        this.description= description;
-    }
-
-    /**
-     * Compares this Genre with another object for equality.
-     * Two genres are equal if they have the same ID, name, and description.
-     *
-     * @param o the object to compare with
-     * @return true if the objects are equal, false otherwise
-     */
-    @Override
-    public boolean equals(Object o){
-        if (this == o){
-            return true;
+    public String format() {
+        String formattedText = genreID + ": " + genreName;
+        if (description != null && !description.isEmpty()) {
+            formattedText = formattedText + "\n\t" + description;
         }
-        if(o == null){
-            return false;
-        }
-        if(getClass() != o.getClass()){
-            return false;
-        }
-        Genre other = (Genre) o;
-        return genreID == other.genreID &&
-                Objects.equals(genreName, other.genreName) &&
-                Objects.equals(description,other.description);
+        return formattedText;
     }
+
+
+    public static boolean deepEquals(Genre g1, Genre g2) {
+        return Objects.equals(g1.genreID, g2.genreID)
+                && Objects.equals(g1.genreName, g2.genreName)
+                && Objects.equals(g1.description, g2.description);
+    }
+
     @Override
-    public int hashCode(){
-        return Objects.hash(genreID,genreName, description);
-
+    public int compareTo(Genre g) {
+        if (genreID < g.genreID) {
+            return -1;
+        } else if (genreID > g.genreID) {
+            return 1;
+        }
+        return 0;
     }
-    @Override
-    public String toString(){
-        return "Genre{" + "Genre ID: "+ genreID +" ,Genre Name: "+ genreName + " ,Genre Description: "+ description + "}";
-    }
-
-
 }
