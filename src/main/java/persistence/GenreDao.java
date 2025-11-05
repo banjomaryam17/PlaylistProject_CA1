1a36592be5fcb404cad39c34db89ec72ca63ca02
maryam banjo <mitechnigeria@gmail.com>
2025-11-05 09:53:21 +0000

    Feature: Created GenreDao and added all necessary methods to be implemented

--- src/main/java/persistence/GenreDao.java
+++ src/main/java/persistence/GenreDao.java
@@ -1,4 +1,14 @@
 package persistence;
+import entities.Genre;
+import java.sql.SQLException;
+import java.util.List;
 
 public interface GenreDao {
+    public Genre create(Genre genre) throws SQLException;
+    public Genre findById(int genreID) throws SQLException;
+    public List<Genre> findAll() throws SQLException;
+    public boolean update(Genre genre) throws SQLException;
+    public boolean delete(int genreID) throws SQLException;
+
+
 }
